8a9382358040a424f612ee0e83433d93aff07a40
meustar <meustar829@gmail.com>
2024-06-28 09:53:06 +0900

    번호추가, 목록 구성ê¹

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,27 +1,35 @@
 package org.koreait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
+    List<Integer> numbers = new ArrayList<>();
     private Scanner sc;
 
     public App(Scanner sc) {
         this.sc = sc;
     }
     public void run() {
-        System.out.println("== motivation 실행 ==");
+        System.out.println("== motivation execution ==");
+
+        int lastId = 1;
+
         while (true) {
-            System.out.print("명령어) ");
+            System.out.print("command) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {   // String 끼리의 비교는 서로 주소값을 비교하기 때문에 무한루프에 빠짐.
-                System.out.println("== motivation 종료 ==");
+                System.out.println("== motivation end ==");
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("명력어를 입력해주세요. :)");
                 continue;
             }
+
+
 
             // 찐 기능
             if (cmd.equals("add")) {
@@ -29,8 +37,14 @@ public class App {
                 String motivation = sc.nextLine();
                 System.out.print("source : ");
                 String source = sc.nextLine();
-                System.out.println("~번 motivation이 등록 되었습니다.");
+                System.out.printf("%d번 motivation이 등록 되었습니다.\n", lastId);
+                lastId++;
             // 번호를 구현해보자. 2024_06_27
+            } else if (cmd.equals("list")) {
+                System.out.println("== motivation list ==");
+                System.out.printf("    id    //   motivation   //   source   \n");
+                System.out.println("=".repeat(40));
+
             }
         }
 
